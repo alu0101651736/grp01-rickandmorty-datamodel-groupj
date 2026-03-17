e23cef7df2b8eb92d23e2b19dd045e8cf5b9ab55
Ramon Colomer <alu0101646558@ull.edu.es>
2026-03-17 13:43:34 +0000

    Creadas las interfaces dimension e invento

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,5 +1,10 @@
-import { estadosDimension } from "./types";
+import { estadosDimension, tiposEspecie, tiposInvento } from "./types";
 
+
+
+/**
+ * Interfaz que define los atributos comunes de los objetos en el sistema.
+ */
 export interface IAtributos {
   /** ID unico del objeto */
   id: string;
@@ -8,6 +13,9 @@ export interface IAtributos {
   nombre: string;
 }
 
+/**
+ * Interfaz que define los atributos específicos de una __dimensión__ en el sistema.
+ */
 export interface IDimension extends IAtributos {
   /** Estado actual de la dimension */
   estadoDim: estadosDimension;
@@ -18,3 +26,34 @@ export interface IDimension extends IAtributos {
   /** Descripcion adicional de la dimension */
   descripcion: string;
 }
+
+
+/**
+ * Interfaz que define los atriutos de las __especies__ en el sistema.
+ */
+export interface IEspecie extends IAtributos {
+  /** Origen de la especie - referencia al planeta o dimensión de la especie */
+  origen: string;
+  /** Tipo de especie*/
+  tipo: tiposEspecie;
+  /** Esperanza de vida de la especie */
+  esperanzaVida: number;
+  /** Descripcion adicional de la especie */
+  descripcion: string;
+}
+
+/**
+ * Interfaz que define los atributos de los __inventos__ en el sistema.
+ */
+export interface IInvento extends IAtributos {
+  /** Nombre del inventor del invento - referencia al personaje */
+  inventor: string;
+  /** Tipo de invento */
+  tipo: tiposInvento;
+  /** Nivel de peligro del invento, esta entre 1 y 10 */
+  nivelPeligro: number;
+  /** Descripcion adicional del invento */
+  descripcion: string;
+}
+ 
+
--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,6 @@
 /** Valores posibles para el estado de la dimension */
 export type estadosDimension = "activa" | "destruida" | "en cuarentena";
+/** Tipos de especies */
+export type tiposEspecie = "humanoide" | "amorfo" | "robótico" | "parásito" | "hivemind";
+/** Tipos de inventos */
+export type tiposInvento = "Arma" | "Dispositivo de viaje" | "Biotecnología" | "Objeto cotidiano absurdo";
