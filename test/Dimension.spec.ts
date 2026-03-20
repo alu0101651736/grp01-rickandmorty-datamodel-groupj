97b9a150d00a2355dd5be76a277d7794b122ea3f
Juan Esteban <alu0101592916@ull.edu.es>
2026-03-20 03:05:55 +0000

    fix: se cambiaron los test para coincidir con el camio en el orden de os atributos en el codigo fuente

--- test/Dimension.spec.ts
+++ test/Dimension.spec.ts
@@ -1,11 +1,11 @@
 import { describe, expect, test, beforeEach } from "vitest";
-
 import { Dimension } from "../src/Dimension";
 
 describe("Tests constructor de clase Dimension", () => {
   let dimension: Dimension;
+
   beforeEach(() => {
-    dimension = new Dimension("D1", "C-137", "activa", "Original", 7);
+    dimension = new Dimension("D1", "C-137", "activa", 7, "Original");
   });
 
   test("se crea una dimension", () => {
@@ -13,40 +13,35 @@ describe("Tests constructor de clase Dimension", () => {
   });
 
   test("error de id vacio", () => {
-    expect(() => new Dimension("", "C-137", "activa", "Original", 7)).toThrow(
-      "La ID no puede ser vacia",
-    );
+    expect(() => new Dimension("", "C-137", "activa", 7, "Original"))
+      .toThrow("La ID no puede ser vacia");
   });
 
   test("error de nombre vacio", () => {
-    expect(() => new Dimension("D1", "", "activa", "Original", 7)).toThrow(
-      "El nombre no puede ser vacio",
-    );
+    expect(() => new Dimension("D1", "", "activa", 7, "Original"))
+      .toThrow("El nombre no puede ser vacio");
   });
 
   test("error de descripcion vacia", () => {
-    expect(() => new Dimension("D1", "C-137", "activa", "", 7)).toThrow(
-      "La descripcion no puede estar vacia",
-    );
+    expect(() => new Dimension("D1", "C-137", "activa", 7, ""))
+      .toThrow("La descripcion no puede estar vacia");
   });
 
   test("error de rango (negativo)", () => {
-    expect(
-      () => new Dimension("D1", "C-137", "activa", "Original", -1),
-    ).toThrow("Indice fuera de rango");
+    expect(() => new Dimension("D1", "C-137", "activa", -1, "Original"))
+      .toThrow("Indice fuera de rango");
   });
 
   test("error de rango (positivo)", () => {
-    expect(
-      () => new Dimension("D1", "C-137", "activa", "Original", 11),
-    ).toThrow("Indice fuera de rango");
+    expect(() => new Dimension("D1", "C-137", "activa", 11, "Original"))
+      .toThrow("Indice fuera de rango");
   });
 
   test("atributos de la clase", () => {
     expect(dimension.id).toBe("D1");
     expect(dimension.nombre).toBe("C-137");
-    expect(dimension.descripcion).toBe("Original");
     expect(dimension.estadoDim).toBe("activa");
     expect(dimension.nivelTec).toBe(7);
+    expect(dimension.descripcion).toBe("Original");
   });
 });
